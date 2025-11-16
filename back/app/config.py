d06275cd97f13c7c03535a40584f4865872f6b2c
mdurc <matthewdurcan@icloud.com>
2025-11-15 22:45:58 -0500

    update

--- back/app/config.py
+++ back/app/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Settings:
     ENV: str = os.getenv("ENV", "dev")
     APP_NAME: str = os.getenv("APP_NAME", "MyBackend")
-    PORT: int = int(os.getenv("PORT"))
+    PORT: int = int(os.getenv("PORT", 8000))
     UDP_SERVER_HOST: str = os.getenv("UDP_SERVER_HOST", "129.161.144.78")
     UDP_SERVER_PORT: int = int(os.getenv("UDP_SERVER_PORT", "5002"))
 settings = Settings()
